42c8206eb2f89c35f5f6438eb21d3f064fbc195f
FerFabbiano <ffabbiano@fi.uba.ar>
2024-04-15 23:31:25 -0300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -2,17 +2,5 @@ import os
 import logging
 import time
 
-# Get the absolute path to the current directory
-current_dir = os.path.abspath(os.path.dirname(__file__))
-
-# Specify the log file name and create the absolute path
-log_file_path = os.path.join(current_dir, 'results', 'app.log')
-
-logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
-
-logging.info("Hello World!")
-i = 0
-while i < 100:
-  i += 1
-  
-logging.info("Finished loop")
+print("Hello world from execution")
+print("Finishing execution! Bye!")
